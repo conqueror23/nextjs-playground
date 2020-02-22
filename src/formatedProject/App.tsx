7f0b2570629ffbf9899ea4aa69dc19eddd8d6a26
bruce <wayneborong@gmail.com>
2020-02-22 14:36:35 +1100

    update on project structure in the playground

---
+++ src/formatedProject/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const App: React.FC<any> = () => {
+    return (
+        <div>
+            <h1></h1>
+            
+        </div>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Formated from './formatedProject/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Formated />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
