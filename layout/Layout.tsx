601bb02f23003380df4ad863ae872dbe24f0efc3
Bolong Wang <bolongwang@Bolongs-MBP.modem>
2020-02-22 20:47:55 +1100

    update and synchronise with remote

---
+++ layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+import {Page1,Page2} from '../pages'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+
+
+export default function Layout({}: Props): ReactElement {
+    return (
+        <div>
+            
+        </div>
+    )
+}
